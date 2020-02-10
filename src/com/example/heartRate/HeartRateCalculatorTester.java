fec36d301885ee8e2f87aabc7673764542d0fdc8
Jacek Kania <kjacek31@gmail.com>
2020-02-10 11:10:25 +0000

    add user input

--- src/com/example/heartRate/HeartRateCalculatorTester.java
+++ src/com/example/heartRate/HeartRateCalculatorTester.java
@@ -1,28 +1,31 @@
 package com.example.heartRate;
 
 import java.time.LocalDate;
-
-
-
+import java.util.Scanner;
 
 public class HeartRateCalculatorTester {
 
-	
 	public static void main(String[] args) {
-		
-		HeartRateCalculator heartrate=new HeartRateCalculator("Jacek", "Kania", LocalDate.of(1977, 2, 8));
-		HeartRateCalculator heartrate1=new HeartRateCalculator("Jacek", "Kania", LocalDate.of(1987, 5, 8));
-		
-		
+
+		Scanner keyIn = new Scanner(System.in);
+
+		System.out.println("Please enter yor details");
+		System.out.print("Your name:");
+		String fname = keyIn.nextLine();
+		System.out.print("Your  last name:");
+		String lname = keyIn.nextLine();
+		System.out.print("DOB-day:");
+		int day = keyIn.nextInt();
+		System.out.print("DOB-month:");
+		int month = keyIn.nextInt();
+		System.out.print("DOB-year:");
+		int year = keyIn.nextInt();
+
+		HeartRateCalculator heartrate = new HeartRateCalculator(fname, lname, LocalDate.of(year, month, day));
 		heartrate.displayCustomerDetails();
 		System.out.println("Your age is: " + heartrate.calculateAge());
 		System.out.println("Your maximum hear rate is " + heartrate.calculateHeartRate() + " heart beats per hour");
 		heartrate.targetRate();
-		
-		heartrate1.displayCustomerDetails();
-		System.out.println("Your age is: " + heartrate1.calculateAge());
-		System.out.println("Your maximum hear rate is " + heartrate1.calculateHeartRate() + " heart beats per hour");
-		heartrate1.targetRate();
 
 	}
 
