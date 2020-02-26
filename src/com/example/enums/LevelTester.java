24697585056101638d3a896fd10fdf4bc35aa1f1
Jacek Kania <kjacek31@gmail.com>
2020-02-26 08:40:25 +0000

    enumeration

--- src/com/example/enums/LevelTester.java
+++ src/com/example/enums/LevelTester.java
@@ -3,13 +3,12 @@ package com.example.enums;
 public class LevelTester {
 
 	public static void main(String[] args) {
-		Level level = Level.LEVEL1;
+		Level level = Level.LEVEL3;
 
 		switch (level) {
 		case LEVEL1:
 			System.out.println("This level is 1- " + "Rate: " + level.getRate());
 			break;
-
 		case LEVEL2:
 			System.out.println("This level is 2- " + "Rate: " + level.getRate());
 			break;
